4c9e11f4a55621da9aa1326ae9cfa0619fbf135e
Rob Landley <rob@landley.net>
2011-07-06 07:50:06 -0500

    Add "all" target to build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #/bin/bash
 
-if [ -z "$1" ] || [ ! -x "images/$1/build.sh" ]
+if [ "$1" != "all" ] && [ ! -x "images/$1/build.sh" ]
 then
-  echo "Available images:"
+  echo 'Available images (or "all"):'
   ls images
+  exit 1
 fi
 
-images/"$1"/build.sh
+if [ "$1" != all ]
+then
+  images/"$1"/build.sh
+else
+  for i in $(ls images)
+  do
+    images/"$i"/build.sh || exit 1
+  done
+fi
