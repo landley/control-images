a092c4398610cecdf6fae197155429018b79c160
Rob Landley <rob@landley.net>
2011-09-17 23:08:29 -0500

    Gratuitously breaking the build on big endian mips, when there are no obvious endianness issues, makes no sense.

--- images/lfs-bootstrap/mnt/build/psmisc.sh
+++ images/lfs-bootstrap/mnt/build/psmisc.sh
@@ -3,6 +3,7 @@
 # Another bugfix that you'd think would be a patch, but no...
 
 sed -i 's@#include<sys\/usr.h>@#include <bits\/types.h>\&@' configure &&
+sed -i '/#error /d' src/peekfd.c &&
 
 ./configure --prefix=/usr --disable-nls &&
 make -j $CPUS &&
