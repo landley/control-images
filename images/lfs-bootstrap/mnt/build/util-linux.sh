153469ccde0225ac4e7638608f7f352f87991d04
Rob Landley <rob@landley.net>
2011-11-06 21:08:49 -0600

    Fix a couple problems with the util-linux build.

--- images/lfs-bootstrap/mnt/build/util-linux.sh
+++ images/lfs-bootstrap/mnt/build/util-linux.sh
@@ -2,6 +2,10 @@
 
 sed -e 's@etc/adjtime@var/lib/hwclock/adjtime@g' \
   -i $(grep -rl '/etc/adjtime' .) &&
+# uClibc hasn't got argp.h, and it's the wrong header to include anyway.
+sed -e 's/<argp[.]h>/<errno.h>/' -i configure &&
+# They never tested --disable-nls and use gettext() directly without masking it.
+sed -e '/define _(/{n;/define N_(/a \# define gettext(Text) (Text)' -e '}' -i include/nls.h &&
 mkdir -p /var/lib/hwclock &&
 ./configure --enable-arch --enable-partx --enable-write --disable-nls &&
 make -j $CPUS &&
