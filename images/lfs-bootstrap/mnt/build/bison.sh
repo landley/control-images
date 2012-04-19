ffc29fda17254cf8f34aa302b5200cdcd7d01db1
Rob Landley <rob@landley.net>
2012-04-19 00:10:54 -0500

    Fixes for uClibc 0.9.33.1.

--- images/lfs-bootstrap/mnt/build/bison.sh
+++ images/lfs-bootstrap/mnt/build/bison.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sed -i 's/#ifndef __GLIBC__/#if !defined __GLIBC__ || defined __UCLIBC__/' \
+  lib/spawn.in.h || exit 1
+
 ./configure --prefix=/usr &&
 echo '#define YYENABLE_NLS 1' >> lib/config.h &&
 make -j $CPUS || exit 1
--- images/lfs-bootstrap/mnt/build/m4.sh
+++ images/lfs-bootstrap/mnt/build/m4.sh
@@ -1,1 +1,16 @@
-generic-check.sh
+#!/bin/sh
+
+sed -i 's/#ifndef __GLIBC__/#if !defined __GLIBC__ || defined __UCLIBC__/' \
+  lib/spawn.in.h || exit 1
+
+# Standard install, plus "make check".
+
+./configure --prefix=/usr --bindir=/bin &&
+make -j $CPUS || exit 1
+
+if [ ! -z "$CHECK" ]
+then
+  make check || exit 1
+fi
+
+make install
